75708c01e7d15aab4dbe3f20ad9cef62112ee288
iSmash <nemeth.richo@gmail.com>
2019-09-24 10:47:50 +0100

    rooms#create action added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,6 +8,17 @@ class RoomsController < ApplicationController
   	@room = Room.new
   end
 
+  def create
+  	@room =Room.new(room_params)
+  	if @room.save
+  	  flash[:success] = "Room has been created with name #{@room.name}"
+  	  redirect_to rooms_path
+  	else
+  	  flash[:alert] = "Room hasn't been created"
+  	  render :new
+  	end
+  end
+
   private
 
   def room_params
