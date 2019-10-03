9a23cf5a90743238d9d3ad2575af7f83072ccd52
iSmash <nemeth.richo@gmail.com>
2019-10-03 09:59:58 +0100

    new logic as try

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,20 @@
 class RoomMessagesController < ApplicationController
+
+  def new
+  	@room = Room.find(params[:id])
+  	@room_message = RoomMessage.all
+  	@new_room_message = RoomMessage.new
+  end
+  
+
+  def create
+    @room_message = RoomMessage.create(message_params)
+    redirect_to rooms_path                                   
+  end
+
+  private
+
+  def message_params
+  	params.require(:room_message).permit(:message,:room_id,:user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root controller: :rooms, action: :index
 
+  get 'rooms/:id', to: 'room_messages#new'
+  
   resources :room_messages
   resources :rooms
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+
   
 end
