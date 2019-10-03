d28b136d13702620159df8117ef0f26bc086a406
iSmash <nemeth.richo@gmail.com>
2019-10-03 15:52:29 +0100

    messages controller new and create action skeleton has been setup, redirect has done

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -3,13 +3,16 @@ class RoomMessagesController < ApplicationController
   def new
   	@room = Room.find(params[:id])
   	@room_message = RoomMessage.all.where(room_id:@room)
+  	@current_room = @room.id
   	@new_room_message = RoomMessage.new
   end
   
 
   def create
-    @new_room_message = RoomMessage.create(message_params)
-    redirect_to rooms_path                                   
+    @new_room_message = RoomMessage.new(message_params)
+    @current_room = @new_room_message.room_id
+    @new_room_message.save
+    redirect_to room_path(@current_room)                                   
   end
 
   private
@@ -17,4 +20,5 @@ class RoomMessagesController < ApplicationController
   def message_params
   	params.require(:room_message).permit(:message,:room_id,:user_id)
   end
+
 end
