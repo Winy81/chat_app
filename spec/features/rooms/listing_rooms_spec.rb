f35e79212806f0278ecb8c3a77b178948845bbda
iSmash <nemeth.richo@gmail.com>
2019-10-09 10:09:55 +0100

    logic problem solving on user details and test for user details in the nav.

--- spec/features/rooms/listing_rooms_spec.rb
+++ spec/features/rooms/listing_rooms_spec.rb
@@ -4,6 +4,18 @@ RSpec.feature "Listing Rooms" do
 
   before do
   	@john = User.create(username:"John", email:"john@test.com", password:"password")
+  
+  login_as(@john)
+
+  end
+
+  scenario "no rooms" do
+
+  	visit "/"
+
+  	expect(page).to have_content(@john.username)
+  	expect(page).to have_content(@john.id)
+
   end
 
 end
