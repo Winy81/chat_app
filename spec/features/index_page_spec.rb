272acb76c49b2a8cf11d2c4ed358280d691aeee7
iSmash <nemeth.richo@gmail.com>
2019-10-12 11:38:42 +0100

    index_page_spec#logging in

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -15,4 +15,23 @@ RSpec.feature "Index page" do
 
   end
 
+    scenario "logging in" do
+
+  	visit "/"
+
+  	john = User.create(username:"John", email:"john@test.com", password:"password")
+
+  	fill_in('user_email', :with => 'john@test.com')
+    fill_in('user_password', :with => 'password')
+
+    click_button('Log in')
+
+    expect(page).to have_content(john.username)
+    expect(page).to have_content(john.id)
+    expect(find_link('Logout'))
+    expect(page).to have_content("No Room")
+
+
+  end
+
 end
