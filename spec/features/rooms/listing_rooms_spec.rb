e86ae25bbc9ac0a8f5170da134c6a76abf5a7f23
iSmash <nemeth.richo@gmail.com>
2019-10-12 09:44:36 +0100

    room test with available rooms

--- spec/features/rooms/listing_rooms_spec.rb
+++ spec/features/rooms/listing_rooms_spec.rb
@@ -13,8 +13,21 @@ RSpec.feature "Listing Rooms" do
 
   	visit "/"
 
+  	expect(page).to have_content("No Room")
+
+  end
+
+    scenario "available rooms" do
+
+    room1 = Room.create(name: "Test_Room")
+    room2 = Room.create(name: "Test_Room_2")
+
+  	visit "/"
+
   	expect(page).to have_content(@john.username)
   	expect(page).to have_content(@john.id)
+  	expect(page).to have_content(room1.name)
+  	expect(page).to have_content(room2.name)
 
   end
 
