fa9a216491d56ec78e7a6cb982a996d7ec89869f
iSmash <nemeth.richo@gmail.com>
2019-10-13 15:12:04 +0100

    destroy_room_spec#Delete room successfully

--- spec/features/rooms/destroy_room_spec.rb
+++ spec/features/rooms/destroy_room_spec.rb
@@ -12,4 +12,28 @@ RSpec.feature "Destroy room" do
 
   end
 
+  scenario "Delete room successfully" do
+
+  	rooms = Room.all.count
+
+  	visit "/"
+
+  	expect(page).to have_content(@first_room.name)
+  	expect(page).to have_content(@second_room.name)
+  	expect(rooms).to eq(2)
+
+  	click_link(@first_room.name)
+
+  	expect(page).to have_content("Close the room")
+  	click_link("Close the room")
+
+  	rooms = Room.all.count
+
+  	expect(page).to have_content(@second_room.name)
+  	expect(page).to_not have_content(@first_room.name)
+  	expect(page).to have_content("Room has been closed")
+  	expect(rooms).to eq(1)
+
+  end
+
 end
