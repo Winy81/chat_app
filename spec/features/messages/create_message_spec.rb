b5d53689db5ff9ed3e13dac8320847d9d16eb8a6
iSmash <nemeth.richo@gmail.com>
2019-10-20 14:12:47 +0100

    expand create_message_spec.rb preparation

--- spec/features/messages/create_message_spec.rb
+++ spec/features/messages/create_message_spec.rb
@@ -8,7 +8,14 @@ RSpec.feature "Messages creation" do
   	  @jane = User.create(username:"Jane", email:"jane@test.com", password:"password")
   	  @first_room = Room.create(name: "First Room")
 
+  	  @first_message = RoomMessage.create(room_id: @first_room.id, user_id: @john.id, message: "Test message from John")
+  	  @second_message = RoomMessage.create(room_id: @first_room.id, user_id: @jane.id, message: "Test message from Jane")
+  	  
   	  login_as(@john)
+
+  end
+
+  scenario "create a new message" do
 
   end
 
