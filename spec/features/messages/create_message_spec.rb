8b2732141a8541516ba0715463a14409c67b7733
iSmash <nemeth.richo@gmail.com>
2019-10-20 16:40:51 +0100

    create_message_spec.rb#create a new message

--- spec/features/messages/create_message_spec.rb
+++ spec/features/messages/create_message_spec.rb
@@ -2,20 +2,35 @@ require "rails_helper"
 
 RSpec.feature "Messages creation" do
 
-    before do
+  before do
   	
-  	  @john = User.create(username:"John", email:"john@test.com", password:"password")
-  	  @jane = User.create(username:"Jane", email:"jane@test.com", password:"password")
-  	  @first_room = Room.create(name: "First Room")
+    @john = User.create(username:"John", email:"john@test.com", password:"password")
+    @jane = User.create(username:"Jane", email:"jane@test.com", password:"password")
+    @first_room = Room.create(name: "First Room")
 
-  	  @first_message = RoomMessage.create(room_id: @first_room.id, user_id: @john.id, message: "Test message from John")
-  	  @second_message = RoomMessage.create(room_id: @first_room.id, user_id: @jane.id, message: "Test message from Jane")
-  	  
-  	  login_as(@john)
-
+    @first_message = RoomMessage.create(room_id: @first_room.id, user_id: @john.id, message: "Test message from John")
+    @second_message = RoomMessage.create(room_id: @first_room.id, user_id: @jane.id, message: "Test message from Jane")
+  
+    login_as(@john)
+  
   end
 
   scenario "create a new message" do
+
+  	visit "/"
+
+  	expect(page).to have_content(@first_room.name)
+
+  	click_link(@first_room.name)
+
+  	expect(page).to have_content("#{@first_message.message} - User: #{@john.username}")
+  	expect(page).to have_content("#{@second_message.message} - User: #{@jane.username}")
+
+  	fill_in('room_message_message', :with => 'Third message')
+
+  	click_button("Send")
+
+  	expect(page).to have_content("Third message - User: #{@john.username}")
 
   end
 
