a55ad7774c5259b99653295c5ac2692e8d55a629
iSmash <nemeth.richo@gmail.com>
2019-11-04 16:12:07 +0000

    expanded validation for room

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,6 @@ class Room < ApplicationRecord
 
   has_many :room_messages, dependent: :destroy, inverse_of: :room
   validates :name, presence: true
+  validates :name, length: {minimum: 3, maximum: 30} 
   
 end
