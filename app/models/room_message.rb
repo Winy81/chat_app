b23d42fd6b97be52bd192ec78516c9eea95b1158
iSmash <nemeth.richo@gmail.com>
2019-11-04 15:54:55 +0000

    filter class method been added for messages

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -2,5 +2,9 @@ class RoomMessage < ApplicationRecord
 
   belongs_to :user
   belongs_to :room, inverse_of: :room_messages
+
+  def self.messgage_filter(parameters)
+  	all.where(parameters)
+  end
   
 end
