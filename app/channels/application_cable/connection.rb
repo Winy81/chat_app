7f8e42bfd291f0e94e6970e6e5221776807116d7
iSmash <nemeth.richo@gmail.com>
2019-11-14 10:12:55 +0000

    connect method for action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	
+  	identified_by :current_user
+
+  	def connect
+      self.current_user = find_current_user
+    end
+
   end
 end
