079fcaba86921d4444103e1c7b3dca148e296326
iSmash <nemeth.richo@gmail.com>
2019-11-14 10:53:52 +0000

    find_current_user method setup

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,5 +7,15 @@ module ApplicationCable
       self.current_user = find_current_user
     end
 
+    protected
+
+    def find_current_user
+      if current_user = User.find_by(id: cookies.signed['user.id'])
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
   end
 end
