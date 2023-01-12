01011d0648a7e28faa60a579a3c4624d36df09b7
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-12 13:26:52 +0000

    Add role for user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_111621) do
+ActiveRecord::Schema.define(version: 2023_01_12_110259) do
 
   create_table "room_messages", force: :cascade do |t|
     t.integer "room_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_111621) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
