fe70e450ade7b7b01ff1e42b27febcd6be30e3db
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-14 08:17:08 +0000

    Extra validations for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
 
+  module Role
+    ADMIN = 'admin'.freeze
+    USER = 'user'.freeze
+    GUEST = 'guest'.freeze
+    ALL = [ADMIN, USER, GUEST]
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true, presence: true
   validates_presence_of :role
+  validates_inclusion_of :role, in: Role::ALL
 
 end
