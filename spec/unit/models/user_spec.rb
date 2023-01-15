b9908ef5fc99d5b3f960e2d3c120d2aa20f2616e
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-15 11:07:45 +0000

    Test of User model

--- spec/unit/models/user_spec.rb
+++ spec/unit/models/user_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe User, type: :model do
 
+  describe 'validations' do
+
+    it { is_expected.to validate_presence_of :username }
+    it { is_expected.to validate_presence_of :role }
+
+    it { is_expected.to validate_uniqueness_of :username }
+
+    it { is_expected.to validate_inclusion_of(:role).in?(Role::ALL) }
+
+  end
 end
