330528d2895eb19a58f0643c17ce2a6f6969348e
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-17 09:52:16 +0000

    Validations tests and upgrades on the model of RoomMessage

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -2,8 +2,10 @@ class RoomMessage < ApplicationRecord
 
   belongs_to :user
   belongs_to :room, inverse_of: :room_messages
-  validates :room_messages, presence: true
-  validates :name, length: {minimum: 0, maximum: 3000} 
+
+  validates :room_id, presence: true
+  validates :message, presence: true
+  validates :message, length: {minimum: 0, maximum: 10000}
 
   def self.messgage_filter(parameters)
   	all.where(parameters)
--- spec/unit/models/room_message_spec.rb
+++ spec/unit/models/room_message_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
+require 'rails_helper'
 
 describe RoomMessage, type: :model do
 
+  describe 'validations' do
+
+    it { is_expected.to validate_presence_of :room_id }
+    it { is_expected.to validate_presence_of :message }
+
+    it { is_expected.to validate_length_of(:message).is_at_least(0) }
+    it { is_expected.to validate_length_of(:message).is_at_most(10000) }
+
+  end
 end
