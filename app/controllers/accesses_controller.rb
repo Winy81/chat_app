58db87a199e5e23bb14226203358e92678fd3cf3
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-25 13:02:44 +0000

    Handle add permission for a room

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -1,11 +1,30 @@
 class AccessesController < ApplicationController
 
   def invite
-    redirect_to rooms_path
+    @room = Room.find_by_id(access_params[:room_id])
+    checked_permission_for_update = @room.accesses.where(user_id:current_user.id)
+    if checked_permission_for_update.empty?
+      flash[:alert] = "You are not authorized add permission for this room"
+      redirect_to rooms_path
+    else
+      new_access = @room.accesses.new(user_id:access_params[:invited_user_id])
+      if new_access.save
+        flash[:notice] = "Permission been added for #{new_access.user.username}"
+        redirect_to room_path(@room.id)
+      else
+        flash[:alert] = "Permission couldn't been saved"
+        redirect_to rooms_path
+      end
+    end
   end
 
   def uninvite
     redirect_to rooms_path
+  end
+
+  def access_params
+    params.require(:access_params).permit(:invited_user_id,
+                                          :room_id)
   end
 
 end
