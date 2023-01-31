61e8ad90fda170a54dc5c0fbeb0aa2a0c933c794
Winy81 <Adam.Nemeth@FootballPools.com>
2023-01-31 09:46:03 +0000

    Build up 'Uninvite' process

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -6,7 +6,7 @@ class AccessesController < ApplicationController
     if checked_permission_for_update.empty?
       redirection_for_rooms_path_with_flash(:alert, "You are not authorized add permission for this room")
     else
-      new_access = @room.accesses.new(user_id:access_params[:invited_user_id])
+      new_access = @room.accesses.new(user_id:access_params[:access_for_user_id])
       if new_access.save
         flash[:notice] = "Permission been added for #{new_access.user.username}"
         redirect_to room_path(@room.id)
@@ -17,11 +17,24 @@ class AccessesController < ApplicationController
   end
 
   def uninvite
-    redirection_for_rooms_path_with_flash(:alert, "User access has been removed from this room")
+    access = Access.find_by(user_id:access_params[:access_for_user_id], room_id:access_params[:room_id])
+    if access.present?
+      if access.delete
+        flash[:alert] = "#{access.user.username}'s access has been removed from this room"
+        redirect_to room_path(access_params[:room_id])
+      else
+        flash[:alert] = "Permission has not been deleted for #{access.user.username}"
+        redirect_to room_path(access_params[:room_id])
+      end
+    else
+      flash[:alert] = "Looks like permission already removed for #{access.user.username}"
+      redirect_to room_path(access_params[:room_id])
+    end
   end
 
+
   def access_params
-    params.require(:access_params).permit(:invited_user_id,
+    params.require(:access_params).permit(:access_for_user_id,
                                           :room_id)
   end
 
