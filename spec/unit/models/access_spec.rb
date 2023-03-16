a9ffb8677cd2d5a4e01129be33ba8c93dd40d227
Winy81 <Adam.Nemeth@FootballPools.com>
2023-03-16 16:35:27 +0000

    Add validations tests for Access modell

--- spec/unit/models/access_spec.rb
+++ spec/unit/models/access_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 
 describe Access, type: :model do
 
+  describe 'validations' do
 
-  
+    it { is_expected.to validate_presence_of :room_id }
+    it { is_expected.to validate_presence_of :user_id }
 
+  end
 end
