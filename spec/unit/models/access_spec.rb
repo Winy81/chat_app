6aa5f762cc941b5642502d8969b97d528987a278
Winy81 <Adam.Nemeth@FootballPools.com>
2023-04-11 12:30:18 +0100

    Test cover of Access

--- spec/unit/models/access_spec.rb
+++ spec/unit/models/access_spec.rb
@@ -9,4 +9,20 @@ describe Access, type: :model do
     it { is_expected.to validate_presence_of :user_id }
 
   end
+
+  describe 'under_created_room_limit_by_user?' do
+
+    before do
+      @user = User.create(username:'Test', email:'test@test.com', password:'password', role:'admin')
+      @user.accesses.create(room_id:1)
+      @user.accesses.create(room_id:1)
+    end
+
+    it 'should provede access for the user if he has access for number of rooms under limit' do
+
+      expect(Access.under_created_room_limit_by_user?(@user)).to eq(true)
+
+    end
+
+  end
 end
