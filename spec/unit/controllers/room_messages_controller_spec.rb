97c5440163d22f88c41340d14cfe3c35fd91a8d3
Winy81 <Adam.Nemeth@FootballPools.com>
2023-04-19 08:37:28 +0100

    Preparation of controllers test

---
+++ spec/unit/controllers/room_messages_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe RoomMessagesController, type: :controller do
+
+end
---
+++ spec/unit/controllers/rooms_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe RoomsController, type: :controller do
+
+end
