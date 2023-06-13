90e2590a92679c0d1c562cf2a0a18b326b845872
Winy81 <Adam.Nemeth@FootballPools.com>
2023-06-13 08:35:08 +0100

    Cleaning strings from accesses_controller

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -4,14 +4,14 @@ class AccessesController < ApplicationController
     @room = Room.find_by_id(access_params[:room_id])
     checked_permission_for_update = @room.accesses.where(user_id:current_user.id)
     if checked_permission_for_update.empty?
-      redirection_for_rooms_path_with_flash(:alert, "You are not authorized add permission for this room")
+      redirection_for_rooms_path_with_flash(:alert, I18n.t('accesses.alerts.not_authorized'))
     else
       new_access = @room.accesses.new(user_id:access_params[:access_for_user_id])
       if new_access.save
-        flash[:notice] = "Permission been added for #{new_access.user.username}"
+        flash[:notice] = I18n.t("accesses.notices.permission_added #{new_access.user.username}")
         redirect_to room_path(@room.id)
       else
-        redirection_for_rooms_path_with_flash(:alert, "Permission couldn't been saved")
+        redirection_for_rooms_path_with_flash(:alert, I18n.t('accesses.alerts.not_added'))
       end
     end
   end
@@ -20,14 +20,14 @@ class AccessesController < ApplicationController
     access = Access.find_by(user_id:access_params[:access_for_user_id], room_id:access_params[:room_id])
     if access.present?
       if access.delete
-        flash[:alert] = "#{access.user.username}'s access has been removed from this room"
+        flash[:alert] = I18n.t("#{access.user.username}'s accesses.alerts.not_added")
         redirect_to room_path(access_params[:room_id])
       else
-        flash[:alert] = "Permission has not been deleted for #{access.user.username}"
+        flash[:alert] = I18n.t("accesses.alerts.permission_has_not_been_delete #{access.user.username}")
         redirect_to room_path(access_params[:room_id])
       end
     else
-      flash[:alert] = "Looks like permission already removed for #{access.user.username}"
+      flash[:alert] = I18n.t("accesses.alerts.permission_has_removed_already #{access.user.username}")
       redirect_to room_path(access_params[:room_id])
     end
   end
